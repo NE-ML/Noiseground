95d95de4b4f0d15e287b169204a578edd16f48bc
Лев Барабанщиков <levbara1@gmail.com>
2022-05-28 09:29:58 +0300

    add: proto-style

--- common/main.cpp
+++ common/main.cpp
@@ -1,9 +1,13 @@
 #include <QApplication>
+#include <QFile>
 
 #include "UIManager.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    QFile file("../UI/style/style.css");
+    file.open(QFile::ReadOnly);
+    a.setStyleSheet(file.readAll());
     UIManager uiManager;
     uiManager.showMainWindow();
     return a.exec();
